df425bfc97a70caefb0a60fe9d573b5c72dde097
Abu Taher <entrptaher@users.noreply.github.com>
2017-11-13 09:51:03 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const app = express();
-const automation = require("./automation");
+const automation = {
+	puppeteer: require("./puppeteer"),
+	nightmare: require("./nightmare")
+};
 const port = process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
 	res.send({ hello: "world" });
 });
 
-app.get("/open", async (req, res) => {
-	let title = await automation.openSite();
+app.get("/:engine/open/:url", async (req, res) => {
+	let title = await automation[req.params.engine].openSite(
+		`http://${req.params.url}`
+	);
 	res.send({ title });
 });
 
-app.get("/data", async (req, res) => {
-	let data = await automation.getData();
+app.get("/:engine/data", async (req, res) => {
+	let data = await automation[req.params.engine].getData();
 	res.send(data);
 });
 
-app.get("/close", async (req, res) => {
-	await automation.closeSite();
+app.get("/:engine/close", async (req, res) => {
+	await automation[req.params.engine].closeSite();
 	res.send(`Browser Closed`);
 });
 
